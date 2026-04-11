a676e78815af039d3a87af31c039dacfd1889b7b
bora <bora>
2026-04-12 02:05:28 +0300

    commit

--- Bonime/View/FlowLayout.swift
+++ Bonime/View/FlowLayout.swift
@@ -3,33 +3,54 @@ import SwiftUI
 struct FlowLayout: Layout {
     var spacing: CGFloat
     
-    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout()) -> CGSize {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
         let containerWidth = proposal.width ?? .infinity
-        
         var totalHeight: CGFloat = 0
-        var lineWidth: CGFloat = 0
-        var lineHeight: CGFloat = 0
+        var currentLineWidth: CGFloat = 0
+        var currentLineHeight: CGFloat = 0
+        var maxRowWidth: CGFloat = 0
         
         for subview in subviews {
             let viewSize = subview.sizeThatFits(.unspecified)
             
-            if lineWidth + viewSize.width > containerWidth {
-                totalHeight += lineHeight + spacing
-                lineWidth = 0
-                lineHeight = 0
+            // Mathematically precise wrap check
+            if currentLineWidth + viewSize.width > containerWidth && currentLineWidth > 0 {
+                totalHeight += currentLineHeight + spacing
+                maxRowWidth = max(maxRowWidth, currentLineWidth - spacing)
+                currentLineWidth = 0
+                currentLineHeight = 0
             }
             
-            lineWidth += viewSize.width + spacing
-            
-            lineHeight = max(lineHeight, viewSize.height)
+            currentLineWidth += viewSize.width + spacing
+            currentLineHeight = max(currentLineHeight, viewSize.height)
         }
         
+        // Account for the final line
+        maxRowWidth = max(maxRowWidth, currentLineWidth - spacing)
+        let finalHeight = totalHeight + currentLineHeight
         
-        
-        return .zero
+        return CGSize(width: maxRowWidth, height: finalHeight)
     }
     
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        return
+        var x = bounds.minX
+        var y = bounds.minY
+        var currentLineHeight: CGFloat = 0
+        
+        for subview in subviews {
+            let viewSize = subview.sizeThatFits(.unspecified)
+            
+            // Wrap if the view exceeds the bounding box width
+            if x + viewSize.width > bounds.maxX && x > bounds.minX {
+                x = bounds.minX
+                y += currentLineHeight + spacing
+                currentLineHeight = 0
+            }
+            
+            subview.place(at: CGPoint(x: x, y: y), proposal: .unspecified)
+            
+            x += viewSize.width + spacing
+            currentLineHeight = max(currentLineHeight, viewSize.height)
+        }
     }
 }
